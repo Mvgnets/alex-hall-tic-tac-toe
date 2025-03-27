fc399c2f57bf051e66d1a05d3790262e10e1d64e
Alex Hall <alexander.hall@clarionhg.com>
2025-03-27 15:11:01 +0000

    feature: dark mode test

--- src/app/components/rotatePhone.tsx
+++ src/app/components/rotatePhone.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function RotatePhone() {
   return (
-    <div className="flex flex-col mt-4 min-h-screen text-slate-700 text-center gap-4">
-      <h1 className="text-2xl font-bold">
+    <div className="dark:text-white flex flex-col mt-4 min-h-screen text-slate-700 text-center gap-4">
+      <h1 className="text-2xl font-bold ">
         Please rotate your device to landscape mode
       </h1>
       <Image
@@ -11,7 +11,7 @@ export default function RotatePhone() {
         alt="icon"
         width={100}
         height={100}
-        className={"self-center"}
+        className={"self-center dark:bg-white"}
       />
     </div>
   );
